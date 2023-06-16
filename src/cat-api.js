75be9662783c68b53f7771f88435ca13372b32bf
Bubl1k <122160505+Yur1yBubl1k@users.noreply.github.com>
2023-06-16 22:27:34 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,16 +2,16 @@ import Notiflix from 'notiflix';
 
 
 
-export function fetchBreeds() {
+export async function fetchBreeds() {
     const url = 'https://api.thecatapi.com/v1/breeds';   
-  return fetch(url).then(response => response.json());
+  return await fetch(url).then(response => response.json());
   }
 
-  export function fetchCatByBreed(breedId) {
+  export  async function fetchCatByBreed(breedId) {
     const api_key = "live_vKHLYXpTMMrCDzRDlFNFPH8EzcVTR4Mr6oriGMeASnDRekm6E3kJopGhheUEsapq";
 
     const urlImg = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
     
-    return fetch(urlImg, { headers: { 'x-api-key': api_key } }).then(response => response.json())
+    return await fetch(urlImg, { headers: { 'x-api-key': api_key } }).then(response => response.json())
     
 }      
